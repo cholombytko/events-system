7319941a17df9f8f2521034f8cc6b8cb76ca36b4
kcholombytko <funnygungan@gmail.com>
2025-02-24 14:12:13 +0200

    feat: implement prisma service, also add pino-logger for better logging

--- fb-collector-service/src/app.module.ts
+++ fb-collector-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule.forRoot(), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
